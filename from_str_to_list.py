b76533d8ec9e0755c8c7529f4b9df1021c225bd2
funggtopp <fangtao526@gmail.com>
2024-10-14 12:55:04 +0800

    增加一点点兼容性

--- from_str_to_list.py
+++ from_str_to_list.py
@@ -1,6 +1,6 @@
 def from_str_to_list( mystr = 'ok',sep=','):
     '''
-    written by 方涛，20241013
+    written by 方涛，20241014
     从excel或粘贴板中直接粘贴过来的字符串，带分隔符（只支持，和空格）的转换成列表形式
 
     换行符、英文引号 均作删除处理，空格不作为分隔符时也删除
@@ -19,12 +19,18 @@ def from_str_to_list( mystr = 'ok',sep=','):
         列表list
     '''
     import re
+
+    #处理一些莫名其妙的，
+    mystr = re.sub('],\\n +dtype=.*','',mystr)
+    mystr = re.sub('Index\\(\\[','',mystr)
+    mystr = re.sub('\\\\n','',mystr)
+    
     if sep == ',':
-        mystr = re.sub('[  　\n"]','',mystr)
+        mystr = re.sub('[  　\n"\']','',mystr)
         mystr = re.sub('[\t+,+，+]',',',mystr)
         mystr = re.sub(',+',',',mystr)
     if sep == ' ' or sep == '　':
-        mystr = re.sub('[\n"]','',mystr)
+        mystr = re.sub('[\n"\']','',mystr)
         mystr = re.sub('[\t+ +　+]',',',mystr)
         mystr = re.sub(',+',',',mystr)
 
